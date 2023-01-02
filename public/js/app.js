6602b50a9f1968dff8ba12d6b64398a8a65eb22c
afaq <afaqak>
2023-01-03 01:15:50 +0500

    fetching

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
 console.log('Loaded the client side javascript');
+// fetch('http://localhost:4000/weather?address=pakistan').then(res=>res.json().then(data=>{
+//     if(data.errorMessage){
+//         console.error(data.errorMessage);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// }))
+
+const weatherForm=document.querySelector('form');
+const search=document.querySelector('input');
+weatherForm.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log('submitted');
+    const location=search.value;
+    console.log(location);
+
+    fetch(`http://localhost:4000/weather?address=${location}`).then(res=>res.json().then(data=>{
+     if(data.errorMessage){
+         console.error(data.errorMessage);
+     }
+    else{
+         console.log(data);
+    }
+ }))
+
+})
