a69542849e06e216d786c8ee6e3e753ea4db370c
afaq <afaqak>
2023-01-02 20:49:41 +0500

    added another route

--- src/app.js
+++ src/app.js
@@ -1,91 +1,101 @@
-const express=require('express');
-const path=require('path');
-const hbs=require('hbs');
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 //paths for express config
-const app=express(express);
-const publicDirectory=path.join(__dirname,'../public');
-const viewsDirectory=path.join(__dirname,'../templates/views');
-const partialsPath=path.join(__dirname,'../templates/partials');
+const app = express(express);
+const publicDirectory = path.join(__dirname, '../public');
+const viewsDirectory = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //setup handlebars and views locations
-app.set('view engine','hbs');
-app.set('views',viewsDirectory);
+app.set('view engine', 'hbs');
+app.set('views', viewsDirectory);
 hbs.registerPartials(partialsPath);
 
 //set static directory to serve
 app.use(express.static(publicDirectory));
 
 //routes requests
-app.get('',(req,res)=>{
-    res.render('index',{
-        name:'afaq',
-        title:'Weather'
+app.get('', (req, res) => {
+    res.render('index', {
+        name: 'afaq',
+        title: 'Weather'
     })
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about',
-    {
-        title:"hospital",
-        name:"afaq"
+        {
+            title: "hospital",
+            name: "afaq"
+        })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'helpmee',
+        name: 'afaq'
     })
 })
 
-app.get('/help',(req,res)=> {
-    res.render('help',{
-        title:'helpmee',
-        name:'afaq'
-    })
-})
 
-
-app.get('/weather',(req,res)=>{
-    if(!req.query.address)
-    {
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
         return res.send({
-            errorMessage:'You must have an address'
+            errorMessage: 'You must have an address'
         })
     }
     res.send({
-        products:'hello products',
-        address:req.query.address
+        products: 'hello products',
+        address: req.query.address
     })
 })
 
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        errorMessage:"help article not found",
-        name:"Afaq"
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: "help article not found",
+        name: "Afaq"
     })
-    setTimeout(()=>{
+    setTimeout(() => {
         res.redirect('/help');
-    },2000)
+    }, 2000)
 })
 
-app.get('/products',(req,res)=>{
-    if(!req.query.search)
-    {
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
         return res.send({
-            errorMessage:'There is a possible error with your query'
+            errorMessage: 'There is a possible error with your query'
         })
     }
     res.send({
-        products:[]
+        products: []
     })
 })
 
 
-app.get('*',(req,res)=>{
-    res.render('404',{
-        title:'404',
-        errorMessage:"page not found",
-        name:"Afaq"
+app.get('/adresses', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            errorMessage: 'There is a possible error with your query'
+        })
+    }
+    res.send({
+        products: []
     })
 })
 
-app.listen(4000,()=>{
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: "page not found",
+        name: "Afaq"
+    })
+})
+
+app.listen(4000, () => {
     console.log('running');
 });
 
